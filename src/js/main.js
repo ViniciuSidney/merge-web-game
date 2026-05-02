a5f2a0691cbe85a18215977ac74c979d37702664
Vinícius Sidney <viniciusidney0@gmail.com>
2026-05-02 01:13:02 -0300

    Refatoração do código: extração das configurações de upgrades para um novo arquivo upgradeConfig.js, melhorando a organização e a modularidade

--- src/js/main.js
+++ src/js/main.js
@@ -117,52 +117,7 @@ let saveTextTimer = null;
 // 4. CONFIGURAÇÕES DE UPGRADES
 // ===============================
 
-const UPGRADE_CONFIGS = {
-   spawnSpeed: {
-      name: 'Esteira mais rápida',
-      description:
-         'Diminui o tempo de espera para gerar novos objetos em 0.1s por nível.',
-      baseCost: 2000,
-      multiplier: 2,
-      tenMultiplier: 4,
-   },
-
-   startLevel: {
-      name: 'Forma melhorada',
-      description: 'Faz com que os objetos comecem 1 nível acima.',
-      baseCost: 5000,
-      multiplier: 5,
-      tenMultiplier: 10,
-   },
-
-   doubleSpawn: {
-      name: 'Entrega dupla',
-      description:
-         'Aumenta em 2% por nível a chance de nascerem 2 objetos ao mesmo tempo.',
-      baseCost: 1500,
-      multiplier: 2,
-      tenMultiplier: 4,
-   },
-
-   doubleMoney: {
-      name: 'Momento valioso',
-      description:
-         'Aumenta em 2% por nível a chance de um objeto gerar 2x polígonos naquele momento.',
-      baseCost: 2000,
-      multiplier: 2,
-      tenMultiplier: 4,
-   },
-
-   goldenChance: {
-      name: 'Objeto dourado',
-      description:
-         'Aumenta em 2% por nível a chance de nascer um objeto dourado.',
-      baseCost: 10000,
-      multiplier: 4,
-      tenMultiplier: 8,
-      golden: true,
-   },
-};
+import { UPGRADE_CONFIGS } from './upgradeConfig.js';
 
 function createInitialUpgrades() {
    return Object.fromEntries(
